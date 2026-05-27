6235ea39d294b07b67ff67a2713c0c7aba676bff
gusgaaa <gustavo.queirozsa@gmail.com>
2026-05-27 15:05:12 -0300

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+// Links CSV já integrados para a Copa 365.
+// O site vai buscar os dados direto do Google Sheets publicado na Web.
+window.COPA365_CONFIG = {
+  jogadoresCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxrPmMeJs6y5i3xv1wY1j9QGoape69hlESPZm0efI-zP5B5Gn-d0MdGsepnFaXUwupCBukoSNOImU5/pub?gid=1431934240&single=true&output=csv",
+  partidasCsv: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQxrPmMeJs6y5i3xv1wY1j9QGoape69hlESPZm0efI-zP5B5Gn-d0MdGsepnFaXUwupCBukoSNOImU5/pub?gid=1844115525&single=true&output=csv"
+};
